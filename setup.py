4971d6e51bf5c67e3ab7eecd832618477fc31e02
Dominic Mortlock <dmortlock@lyft.com>
2019-01-31 00:59:50 -0800

    Add dependency file for pip

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='too_many_repos',
+    description='A pygments lexer for pytest output.',
+    url='https://github.com/locknic/too-many-repos',
+    version='0.0.1',
+    author='Dominic Mortlock',
+    author_email='dom.mortlock@gmail.com',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+    ],
+    install_requires=['mypy'],
+)
